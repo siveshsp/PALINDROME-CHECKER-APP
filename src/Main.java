17de8c8af6d33007d8326ef734a8d73e8f150dd4
siveshsp <siveshcs@gmail.com>
2026-02-27 14:50:17 +0530

    UC5: Refactor palindrome logic into method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -8,17 +9,21 @@ public class Main {
         System.out.print("Enter a sentence: ");
         String input = scanner.nextLine();
 
-        // Remove spaces and convert to lowercase
-        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
-
-        String reversed = new StringBuilder(cleaned).reverse().toString();
-
-        if(cleaned.equals(reversed)) {
+        if (isPalindrome(input)) {
             System.out.println("It is a Palindrome");
         } else {
             System.out.println("It is NOT a Palindrome");
         }
 
         scanner.close();
+    }
+
+    public static boolean isPalindrome(String input) {
+
+        String cleaned = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        return cleaned.equals(reversed);
     }
 }
